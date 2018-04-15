82535f7962b530babd43ec2e984c93f9325b338e
藤原裕貴 <yuuki@yuuki-mac.local>
2018-04-15 17:32:02 +0900

    Add say_respond method

--- 2018/Slackbot/MySlackBot.rb
+++ 2018/Slackbot/MySlackBot.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'sinatra'
@@ -5,7 +6,11 @@ require 'SlackBot'
 
 class MySlackBot < SlackBot
   # cool code goes here
-  
+  def say_respond(params, username: Bot)
+    text = params[:text].match(/「(.*)」と言って/)
+    
+    return {text: #{text}}.merge(option).to_json
+  end
 end
 
 slackbot = MySlackBot.new
@@ -18,5 +23,11 @@ end
 
 post '/slack' do
   content_type :json
-  slackbot.naive_respond(params, username: "Bot")
+  if (prams[:text] =~ /「.*」と言って/) then
+    slackbot.respond(params, username: "FBot")
+  elsif () then
+    #My api method
+  else
+    slackbot.naive_respond(params, username: "FBot")
+  end
 end
