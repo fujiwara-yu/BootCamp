978749c612a7f1abd6f7bca0cd572546e2b9cd1e
Yuuki Fujiwara <fujiwara2018@s.okayama-u.ac.jp>
2018-04-16 18:22:22 +0900

    Fix say_respond

--- 2018/Slackbot/MySlackBot.rb
+++ 2018/Slackbot/MySlackBot.rb
@@ -6,11 +6,17 @@ require 'SlackBot'
 
 class MySlackBot < SlackBot
   # cool code goes here
-  def say_respond(params, username: Bot)
+  def say_respond(params, options = {})
     text = params[:text].match(/「(.*)」と言って/)
-    
-    return {text: #{text}}.merge(option).to_json
+    return {text: text[1]}.merge(options).to_json
+ 
+   # return {text: text[1]}.to_json
   end
+
+  def my_api_respond(params, options = {})
+    
+  end
+
 end
 
 slackbot = MySlackBot.new
@@ -23,11 +29,12 @@ end
 
 post '/slack' do
   content_type :json
-  if (prams[:text] =~ /「.*」と言って/) then
-    slackbot.respond(params, username: "FBot")
-  elsif () then
+  if (params[:text] =~ /「.*」と言って/) then
+    slackbot.say_respond(params, username: "FBot")
+  #elsif () then
     #My api method
   else
     slackbot.naive_respond(params, username: "FBot")
   end
 end
+
